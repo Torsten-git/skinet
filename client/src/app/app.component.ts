f9fa47c52fd7b688f768da67355ff2b81855a0c8
Torsten Feier <Torsten@git.local>
2021-07-01 13:02:08 +0200

    End of section 8 - unvollst

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination'; 
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Skinet';
+
+export class AppComponent implements OnInit {
+    
+    title = 'Skinet';
+    products: IProduct[] | undefined;
+    //products: any[];
+
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any ) => {
+        this.products = response.data;
+    console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
+
