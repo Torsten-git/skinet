750ca1f244fa4c8f5752424a670e6c5c4cae7584
Torsten Feier <Torsten@git.local>
2021-07-12 18:17:24 +0200

    End of section 14

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from 'src/app/basket/basket.service';
 import { ShopService } from './../shop.service';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
@@ -11,13 +12,31 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+  constructor(private shopService: ShopService,
+              private activateRoute: ActivatedRoute, 
+              private bcService: BreadcrumbService, 
+              private basketService: BasketService) {
     this.bcService.set('@productDetails',' ');
    }
 
   ngOnInit(): void {
     this.loadProduct();
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if ( this.quantity > 1){
+      this.quantity--;
+    }
   }
 
   loadProduct(){
