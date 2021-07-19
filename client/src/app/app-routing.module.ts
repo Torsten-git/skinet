7c019a7502a699932218606188619426dbf026b8
Torsten Feier <Torsten@git.local>
2021-07-19 14:31:59 +0200

    End of section 17

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 
 const routes: Routes = [
@@ -13,7 +14,13 @@ const routes: Routes = [
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
   {path: 'shop', loadChildren: () =>import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}}, 
   {path: 'basket', loadChildren: () =>import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}}, 
-  {path: 'checkout', loadChildren: () =>import('./checkout/checkout.module').then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}}, 
+  { path: 'checkout',
+    canActivate: [AuthGuard],
+      loadChildren: () =>import('./checkout/checkout.module')
+      .then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}}, 
+ 
+  {path: 'account', loadChildren: () =>import('./account/account.module').then(mod => mod.AccountModule), data: {breadcrumb: {skip: true}}}, 
+ 
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
